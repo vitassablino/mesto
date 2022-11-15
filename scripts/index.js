6ac37344d49c10ffa94ddd3fb1c3a9d6005eefac
Viktor Akula <vitassablino1@gmail.com>
2022-11-15 11:54:08 +0300

    2 итерация

--- scripts/index.js
+++ scripts/index.js
@@ -1,50 +1,44 @@
-let editButton = document.querySelector(".profile__edit-button");
-let popup = document.querySelector(".popup");
-let closeButton = document.querySelector(".popup-container__close-button");
-let background = document.querySelector(".popup-container");
-let saveButton = document.querySelector(".popup__save-button");
-let inputName = document.getElementById("name");
-let name = document.querySelector(".profile__name");
-let inputDesсription = document.getElementById("description");
-let description = document.querySelector(".profile__description");
-let likes = document.querySelectorAll(".element__like-button");
-let popupContainer = document.querySelector(".popup-container__form");
-let popupForm = document.querySelector(".popup");
-
+const editButton = document.querySelector(".profile__edit-button");
+const popup = document.querySelector(".popup");
+const closeButton = document.querySelector(".popup-container__close-button");
+const background = document.querySelector(".popup-container");
+const saveButton = document.querySelector(".popup__save-button");
+const inputName = document.getElementById("name");
+const name = document.querySelector(".profile__name");
+const inputDesсription = document.getElementById("description");
+const description = document.querySelector(".profile__description");
+const likes = document.querySelectorAll(".element__like-button");
+const popupContainer = document.querySelector(".popup-container__form");
+const popupForm = document.querySelector(".popup");
 
 editButton.addEventListener("click", function (open) {
-  background.classList.add("popup-container_status_active");
-  background.classList.remove("popup-container_status_inactive");
+  background.classList.remove("popup-container_inactive");
   inputName.value = name.textContent;
   inputDesсription.value = description.textContent;
 });
 
 closeButton.addEventListener("click", function (close) {
-  background.classList.add("popup-container_status_inactive");
-  background.classList.remove("popup-container_status_active");
+  background.classList.add("popup-container_inactive");
 });
 
 popupForm.addEventListener("submit", function (save) {
   save.preventDefault(); // без этой функции при нажатии на кнопку  страница перезагружается
   name.textContent = inputName.value;
   description.textContent = inputDesсription.value;
-  background.classList.add("popup-container_status_inactive");
-  background.classList.remove("popup-container_status_active");
+  background.classList.add("popup-container_inactive");
 });
 
-/* likes.forEach((button) => {
+likes.forEach((button) => {
   button.addEventListener("click", function (like) {
-    button.classList.toggle("element__like-button_activity_active");
-    button.classList.toggle("element__like-button_activity_inactive");
+    button.classList.toggle("element__like-button_active");
   });
-}); */
+});
 
 document.addEventListener("click", function (close) {
   var clickPopup = close.composedPath().includes(popupContainer);
   var clickEdit = close.composedPath().includes(editButton);
- 
-      if (!clickEdit && !clickPopup) {
-        background.classList.add("popup-container_status_inactive");
-        background.classList.remove("popup-container_status_active");
-      };
+
+  if (!clickEdit && !clickPopup) {
+    background.classList.add("popup-container_inactive");
+  }
 });
