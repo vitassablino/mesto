f5b4970e62d43194fb6af8d48206ed4761b64a97
Viktor Akula <vitassablino1@gmail.com>
2023-02-13 14:05:04 +0300

    fixed: отредактировано закрытие попама с формами ввода

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -29,7 +29,6 @@ export default class PopupWithForm extends Popup {
     this._popup.addEventListener("submit", (evt) => {
       evt.preventDefault();
       this._handleSubmitForm(this._getInputValues());
-      this.close();
     });
   }
 
